6bafb7192445564eadc6d3909ed339a03e041311
Adam McGurk <mcgurk@mcgurkconsulting.com>
2019-09-25 09:11:12 -0600

    Got rid of the service worker stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,3 @@
-// PWA stuff
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then(reg => {
-      // Registration was successful
-      console.log(`Check out the GitHub Repo for this PWA at: https://github.com/ShineSolar/EmailSignature`);
-    }, err => {
-      // registration failed :(
-      console.log(`ServiceWorker registration failed: ${err}`);
-      // log this error
-    });
-  });
-}
-
 // Adding the error validation
 const inputs = document.querySelectorAll('input, select');
 const inputLength = inputs.length;

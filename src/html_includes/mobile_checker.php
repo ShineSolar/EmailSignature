7395d70e9998467c5f290b441fc8ba7860c15640
Adam McGurk <eldermcgurk@gmail.com>
2020-12-01 07:27:02 -0700

    Got rid of a lot of PHP code

--- src/html_includes/mobile_checker.php
+++
@@ -1,6 +0,0 @@
-<section class='no-mobile' role="region" aria-label="no-mobile">
-	<h1 style='color: #485d6c'>Darn! :(</h1>
-	<p>Because of the type of signature we're creating, we can't offer a mobile version of this tool...yet :).</p>
-	<p>Until then, open this same site up on your computer and it will work perfectly!</p>
-	<a href='https://www.youtube.com/watch?v=5pGRWepi5TM' title='Watch the video tutorial!' style='background-color: #485d6c;' class='button' rel='noopener' target='_blank'><img src='/assets/white-video-icon.svg' title='Watch the tutorial' alt='White box with a white play button in the middle of it'> Click here for instructions</a>
-</section>
